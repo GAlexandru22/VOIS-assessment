567b42e36c3cf74fd785f7f557db78eb4b1b88b7
GAlexandru22 <georgescu.alexandru22@gmail.com>
2024-04-11 00:00:01 +0300

    Training test for the first model

--- data_processing.py
+++ data_processing.py
@@ -2,27 +2,17 @@ import pandas as pd
 import numpy as np
 
 
+csv_file = pd.read_csv("./data/data.csv").values
 
-def main():
+# Shuffling the data to not overfeed our model
+np.random.shuffle(csv_file)
+#Prepare the data from the labels
+x_data = np.concatenate((csv_file[:, 0].reshape(-1, 1), csv_file[:, 2:9], csv_file[:, 10].reshape(-1, 1)),axis=1)
+y_data = csv_file[:, 9]
 
-    #Reading the data
-    csv_file = pd.read_csv("./data/data.csv").values
+#Separate the training data from the test data
+x_train = x_data[:int(0.8 * len(x_data)), :]
+y_train = y_data[:int(0.8 * len(y_data))]
 
-    # Shuffling the data to not overfeed our model
-    np.random.shuffle(csv_file)
-    c = csv_file[:, :9]
-    d = csv_file[:, 10:]
-    #Prepare the data from the labels
-    x_data = np.concatenate((csv_file[:, :9], csv_file[:, 10].reshape(-1, 1)),axis=1)
-    y_data = csv_file[:, 9]
-
-    #Separate the training data from the test data
-    x_train = x_data[:int(0.8 * len(x_data)), :]
-    y_train = y_data[:int(0.8 * len(y_data))]
-
-    x_test = x_data[int(0.8 * len(x_data)):, :]
-    y_test = y_data[int(0.8 * len(y_data)):]
-
-    
-if __name__ == '__main__':
-    main()
+x_test = x_data[int(0.8 * len(x_data)):, :]
+y_test = y_data[int(0.8 * len(y_data)):]
--- models/MHG_model.py
+++ models/MHG_model.py
@@ -4,19 +4,18 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 import os
 
-checkpoint_path = 'checkpoints/MHG.ckpt'
+checkpoint_path = './models/checkpoints/MHG.ckpt'
 cp_dir = os.path.dirname(checkpoint_path)
 def modelDefinition():
 
-    model = Sequential(
-        # adding a Dense input layer
-        Dense(8, input_dim=13, kernel_initializer='normal', activation='relu'),
-        # adding an output layer
-        Dense(8, kernel_initializer='normal'),
-    )
+    model = Sequential()
 
+    # adding a Dense input layer
+    model.add(Dense(16, kernel_initializer='normal', activation='relu'))
+    # adding an output layer
+    model.add(Dense(16, kernel_initializer='normal'))
     #compiling the model using the optimizer Adam and the loss function MSE
-    model.compile(loss='mse', optimizer='adam')
+    model.compile(loss="mean_squared_error", optimizer='adam')
 
     return model
 
@@ -25,7 +24,7 @@ def trainModel(x_train, y_train):
     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, verbose= 1)
 
     model = modelDefinition()
-    model.fit(x_train, y_train, epochs= 100, validation_data=(x_train, y_train), callbacks=[cp_callback])
+    model.fit(x_train, y_train, epochs=10, batch_size=16, verbose=2, callbacks=[cp_callback])
     return model
 
 
