301abc5cefb962c7a36b229eaee3a7906de852ee
GAlexandru22 <georgescu.alexandru22@gmail.com>
2024-04-11 01:14:14 +0300

    Final push

--- models/MHG_model.py
+++ models/MHG_model.py
@@ -19,12 +19,13 @@ def modelDefinition():
 
     return model
 
-def trainModel(x_train, y_train):
+def trainModel(x_train, y_train,x_test, y_test):
 
     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, verbose= 1)
 
     model = modelDefinition()
-    model.fit(x_train, y_train, epochs=10, batch_size=16, verbose=2, callbacks=[cp_callback])
+    model.fit(x_train, y_train, epochs=10, batch_size=16, validation_data=(x_test,y_test), verbose=2, callbacks=[cp_callback])
+
     return model
 
 
